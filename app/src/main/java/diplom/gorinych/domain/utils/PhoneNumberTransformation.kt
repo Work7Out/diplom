3518bfdb35802b0490079bf6b72eaae5c862daa6
hayarov <edurda77@gmail.com>
2023-12-28 08:57:56 +0500

    correct transform phone

--- app/src/main/java/diplom/gorinych/domain/utils/PhoneNumberTransformation.kt
+++ app/src/main/java/diplom/gorinych/domain/utils/PhoneNumberTransformation.kt
@@ -7,11 +7,51 @@ import androidx.compose.ui.text.input.VisualTransformation
 
 class PhoneNumberTransformation : VisualTransformation {
     override fun filter(text: AnnotatedString): TransformedText {
-        return phoneNumFilter(text)
+        return phoneNumFilterBel(text)
     }
 }
 
-private fun phoneNumFilter(text: AnnotatedString): TransformedText {
+private fun phoneNumFilterBel(text: AnnotatedString): TransformedText {
+
+    // +XXX(XX)_XXX_XX_XX
+    val trimmed = if (text.text.length >= 12) text.text.substring(0..11) else text.text
+    var out = ""
+    for (i in trimmed.indices) {
+        if (i==0) out += "+"
+        if (i==3) out += "("
+        out += trimmed[i]
+        if (i==4) out +=") "
+        if (i==6 || i==8 ) out += " "
+
+    }
+
+    val phoneNumberOffsetTranslator = object : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int {
+            if (offset <= 0) return offset
+            if (offset <= 3) return offset +1
+            if (offset <= 4) return offset +2
+            if (offset <= 7) return offset +4
+            if (offset <= 9) return offset +5
+            if (offset <= 11) return offset +6
+            return 17
+
+        }
+
+        override fun transformedToOriginal(offset: Int): Int {
+            if (offset <=0) return offset
+            if (offset <=4) return offset -1
+            if (offset <=9) return offset -2
+            if (offset <=12) return offset -4
+            if (offset <=15) return offset -5
+            if (offset <=18) return offset -6
+            return 11
+        }
+    }
+
+    return TransformedText(AnnotatedString(out), phoneNumberOffsetTranslator)
+}
+
+private fun phoneNumFilterRus(text: AnnotatedString): TransformedText {
 
     // +X(XXX)_XXX_XX_XX
     val trimmed = if (text.text.length >= 11) text.text.substring(0..10) else text.text
